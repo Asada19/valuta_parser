2ed76540f958e912ad569d1c86ed69cb0c185c64
Asada19 <livencor@gmail.com>
2022-08-10 12:08:47 +0600

    Update with functions

--- valuta.py
+++ valuta.py
@@ -12,51 +12,74 @@ table = soup.find('table', id='rates_table')
 Заголовок курсов валют
 """
 headers = []
-for i in table.find_all('th'):
-    title = i.text
-    if title in ['USD', 'EURO', 'RUB', 'KZT']:
-        headers.append(title)
-        headers.append('')
-    else:
-        headers.append(title)
+
+def head(headers: list, table):
+    for i in table.find_all('th'):
+        title = i.text
+        if title in ['USD', 'EURO', 'RUB', 'KZT']:
+            headers.append(title)
+            headers.append('')
+        else:
+            headers.append(title)
+
+head(headers, table)
 
 
 """ 
 Колонка банков, цен покупок и продаж 
 """ 
-for i in soup.find_all('thead'):
-    headers2 =[z.text for z in i.find_all('td')]
+def column(soup):
+    for i in soup.find_all('thead'):
+        headers2 =[z.text for z in i.find_all('td')]
+    return headers2 
+
+headers2 = column(soup)
 
 
 """ 
 Список баков и цен на валюты 
 """
 data =[]
-for tr in table.find('tbody').find_all('tr'):
-    dt = []
-    for i in tr:
-        dt.append(i.text)
-    data.append(dt)
+def update_data(data: list):
+    for tr in table.find('tbody').find_all('tr'):
+        dt = []
+        for i in tr:
+            dt.append(i.text)
+        data.append(dt)
     
+update_data(data)
+
 
 """
 Дата последнего обновления данных
 """
 date = []
-a = soup.find('span', class_='refresh').text
-date.append(a)
+def add_date(date: list, soup):
+    a = soup.find('span', class_='refresh').text
+    date.append(a)
 
 
-""" Добавление двух заголовков и даты """
-data.insert(0, headers2)
-data.insert(0, headers)
-data.append(date)
+add_date(date, soup)
+
+
+""" 
+Добавление двух заголовков и даты 
+"""
+def add_columns(data: list):
+    data.insert(0, headers2)
+    data.insert(0, headers)
+    data.append(date)
+
+add_columns(data)
 
 
 """ 
 Запись данных в CSV 
 """
-with open('valuta.csv', 'w') as file:
-    writer = csv.writer(file)
-    for row in data:
-        writer.writerow(row)
+def write_to_csv(data):
+    with open('valuta.csv', 'w') as file:
+        writer = csv.writer(file)
+        for row in data:
+            writer.writerow(row)
+
+write_to_csv(data)
